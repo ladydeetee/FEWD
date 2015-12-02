05b7afccf91cdf60b77b8b6c32057ffc6aaf6d85
ladydeetee <danielleatheroux@gmail.com>
2015-12-02 17:58:29 -0500

    final-ish

--- FINAL PROJECT/danielletheroux.com/help/js/hover.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	if(! /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
-	$("#D").hover(
-    	function() {
-    		$.backstretch("images/capitol_pink.jpg",{fade:300});
-    	} 
-    );	
-
-
